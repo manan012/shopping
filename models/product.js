3cb99982067353f8e2c94e613c7526799f6971af
“manan012” <manangoel012@gmail.com>
2020-04-10 14:10:18 +0530

    mongoose

--- models/product.js
+++ models/product.js
@@ -1,37 +1,27 @@
-const fs = require('fs');
-const path = require('path');
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+const Sequelize = require('sequelize');
 
-const getProductsFromFile = cb => {
+const sequelize = require('../util/database');
 
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            return cb([]);
-        } else {
-            cb(JSON.parse(fileContent));
-        }
-    });
-}
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
 
-module.exports = class Product {
-
-    constructor(title, imageUrl, description, price) {
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
-    }
-
-    save() {
-        getProductsFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        });
-    }
-
-    static fetchAll(cb) {
-        getProductsFromFile(cb);
-    }
-};
+module.exports = Product;
